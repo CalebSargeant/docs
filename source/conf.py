16d530be6dc97d44e12e5ef1b684a68a81275ba4
Caleb Sargeant <4991715+CalebSargeant@users.noreply.github.com>
2021-02-17 07:53:23 +0200

    fix build

--- source/conf.py
+++ source/conf.py
@@ -54,7 +54,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-html_static_path = ['_static']
 
 # Change the master_doc for readthedocs
 master_doc = 'index'
